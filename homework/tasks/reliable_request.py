283a12dad22f3564feb786cdd21bb700f732f231
Venbiess (WSL Ubuntu) <abc@DESKTOP-9K8SE1V>
2025-04-09 01:15:09 +0300

    all tasks resolved

--- homework/tasks/reliable_request.py
+++ homework/tasks/reliable_request.py
@@ -1,11 +1,17 @@
 import abc
-
 import httpx
 
 
 class ResultsObserver(abc.ABC):
+    _data_observed = []
+
     @abc.abstractmethod
-    def observe(self, data: bytes) -> None: ...
+    def observe(self, data: bytes) -> None:
+        self._data_observed.append(data)
+
+    @abc.abstractmethod
+    def get_observed_data(self) -> list[bytes]:
+        return self._data_observed
 
 
 async def do_reliable_request(url: str, observer: ResultsObserver) -> None:
@@ -18,13 +24,21 @@ async def do_reliable_request(url: str, observer: ResultsObserver) -> None:
 
     Все успешно полученные результаты должны регистрироваться с помощью обсёрвера.
     """
+    RETRIES_NUMBER = 5
 
     async with httpx.AsyncClient() as client:
         # YOUR CODE GOES HERE
-        response = await client.get(url)
-        response.raise_for_status()
-        data = response.read()
+        for _ in range(RETRIES_NUMBER):
+            try:
+                response = await client.get(url, timeout=10)
+                response.raise_for_status()
+                data = response.read()
 
-        observer.observe(data)
+                observer.observe(data)
+                break
+            except TimeoutError:
+                continue
+            except Exception:
+                continue
         return
         #####################
